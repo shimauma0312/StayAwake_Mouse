a4272d08efeb1159550a189d600549ac26cdb7ae
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-02-17 01:17:18 +0900

    feat: Add WASD key movement function and mode selection

--- StayAwake_Mouse.ps1
+++ StayAwake_Mouse.ps1
@@ -1,22 +1,54 @@
-write-host "Press CTRL + C to stop"
 Add-Type -AssemblyName System.Windows.Forms
 
 $sleepDurationSeconds = 10
 $minimum = -1
 $maxnum = 1
 
-while ($true) {
-    $currentCursorPosition = [Windows.Forms.Cursor]::Position
-    $randomXDistance = (Get-Random -Minimum $minimum -Maximum $maxnum)  # X
-    $randomYDistance = (Get-Random -Minimum $minimum -Maximum $maxnum)  # Y
-    $randomValue = (Get-Random -Minimum 0 -Maximum 101)
+$mode = Read-Host "Choose mode: wasd or mouse"
+if ($mode -eq "wasd") {
+    Write-Host "Activating wasd mode (Press CTRL + C to stop)"
+    MoveWASD
+}else{
+    Write-Host "Activating mouse mode"
+    MoveCursor
+}
 
-    for ($iteration = 0; $iteration -lt $randomValue; $iteration += 1) {
-        $currentCursorPosition.x += $randomXDistance
-        $currentCursorPosition.y += $randomYDistance
-        [Windows.Forms.Cursor]::Position = $currentCursorPosition
-        Start-Sleep -Milliseconds 100
+function MoveCursor() {
+    while ($true) {
+        $currentCursorPosition = [Windows.Forms.Cursor]::Position
+        $randomXDistance = (Get-Random -Minimum $minimum -Maximum $maxnum)  # X
+        $randomYDistance = (Get-Random -Minimum $minimum -Maximum $maxnum)  # Y
+        $randomValue = (Get-Random -Minimum 0 -Maximum 101)
+
+        for ($iteration = 0; $iteration -lt $randomValue; $iteration += 1) {
+            $currentCursorPosition.x += $randomXDistance
+            $currentCursorPosition.y += $randomYDistance
+            [Windows.Forms.Cursor]::Position = $currentCursorPosition
+            Start-Sleep -Milliseconds 100
+        }
+
+        Start-Sleep -Seconds $sleepDurationSeconds
     }
+}
 
-    Start-Sleep -Seconds $sleepDurationSeconds
+function MoveWASD() {
+    while ($true) {
+        $randomValue = (Get-Random -Minimum 0 -Maximum 101)
+        $randomKey = (Get-Random -Minimum 0 -Maximum 4)
+
+        for ($iteration = 0; $iteration -lt $randomValue; $iteration += 1) {
+            if ($randomKey -eq 0) {
+                [System.Windows.Forms.SendKeys]::SendWait("w")
+            } elseif ($randomKey -eq 1) {
+                [System.Windows.Forms.SendKeys]::SendWait("a")
+            } elseif ($randomKey -eq 2) {
+                [System.Windows.Forms.SendKeys]::SendWait("s")
+            } elseif ($randomKey -eq 3) {
+                [System.Windows.Forms.SendKeys]::SendWait("d")
+            }
+            Start-Sleep -Milliseconds 100
+        }
+
+        Start-Sleep -Seconds $sleepDurationSeconds
+    }
 }
